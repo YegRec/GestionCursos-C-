39fd0574260bdc2fe99998f76427608b3b3e2e55
Yenery Rafael <yeneri@rgr@hotmail.com>
2025-02-24 21:43:47 -0800

    Clase curso implementada y configurada

--- Curso.cs
+++ Curso.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static GestionCursos.Program;
 
 namespace GestionCursos
 {
-    internal class Curso
+    internal class Curso<T> : ICurso<T> where T : Alumno
     {
+        public string Profesor { get; private set; }
+        public string Nombre { get; private set; }
+        public List<T> ListaAlumnos { get; private set; } = new List<T>();
+
+        public void AgregarAlumno(T alumno)
+        {
+            ListaAlumnos.Add(alumno);
+        }
+
+        public void EliminarAlumno(T alumno)
+        {
+            ListaAlumnos.Remove(alumno);
+        }
+
+        public double CalcularPromedioDelCurso()
+        {
+            return ListaAlumnos.Sum(x => x.ObtenerPromedio());
+        }
+
+
+
+
     }
 }
