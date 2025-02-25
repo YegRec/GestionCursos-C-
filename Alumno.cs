baf3456adad23f4d5ccc9a704581f9a0c48ec784
Yenery Rafael <yeneri@rgr@hotmail.com>
2025-02-24 21:19:54 -0800

    Clase Alumno implementada

--- Alumno.cs
+++ Alumno.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static GestionCursos.Program;
+
 
 namespace GestionCursos
 {
-    internal class Alumno
+    internal class Alumno : IAlumno
     {
+        public string Nombre { get; private set; }
+        public int Edad {  get; private set; }
+        public double Promedio { get; private set; }
+
+        public Alumno(string nombre , int edad, double promedio)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            Promedio = promedio;
+        } 
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"\nNombre: {Nombre}\n" +
+                $"Edad: {Edad}\n" +
+                $"Promedio: {Promedio}");
+        }
+
+        public double ObtenerPromedio()
+        {
+            return Promedio;
+        }
+
     }
 }
