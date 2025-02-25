c04ec8c8cc5ce4fede73590aee8c491a7bb2a986
Yenery Rafael <yeneri@rgr@hotmail.com>
2025-02-24 22:04:14 -0800

    Clase GestionCursos implementada. Metodos agregados

--- GestionCursos.cs
+++ GestionCursos.cs
@@ -6,9 +6,31 @@ using System.Threading.Tasks;
 
 namespace GestionCursos
 {
-    internal class GestionCursos
+    internal class GestionCursos<TCurso, TAlumno>
+        where TCurso : Curso<Alumno>
+        where TAlumno : Alumno
     {
+        public List<TCurso> cursos { get; private set; } = new List<TCurso>();
 
+        public void AgregarCurso(TCurso curso)
+        {
+            cursos.Add(curso);
+        }
+
+        public void EliminarCurso(TCurso curso)
+        {
+            cursos.Remove(curso);
+        }
+
+        public void InscribirAlumno(TCurso curso, TAlumno alumno)
+        {
+            curso.AgregarAlumno(alumno);
+        }
+
+        public void RemoverAlumno(TCurso curso, TAlumno alumno)
+        {
+            curso.EliminarAlumno(alumno);
+        }
 
 
     }
