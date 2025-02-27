1a3fd30f990537d801f91de93b991293ce04f464
Yenery Rafael <yeneri@rgr@hotmail.com>
2025-02-27 03:06:03 -0800

    Actualizacion de clase alumno y visualizacion de interfaz agregadas

--- Alumno.cs
+++ Alumno.cs
@@ -22,6 +22,12 @@ namespace GestionCursos
         //alumno se encuentra estudiando. Ya que cada curso tiene un nombre, al momento de asignar
         //el alumno al curso asignaremos el nombre del curso a esta variable.
         public string CursoAsignado { get; private set; }
+        [JsonInclude]
+
+        //Asignaremos una matricula unica a cada estudiante, tomando en cuenta, nombres, edad y 
+        //El curso que estaria cursando el alumno.
+        public string Matricula { get; private set; }
+
 
         public Alumno(string nombre , int edad, double promedio)
         {
@@ -50,11 +56,48 @@ namespace GestionCursos
             Promedio = promedio;
         }
 
-        //Metodo que usaremos para asignar el nombre del curso al alumno.
-        public void AsignarCurso(string curso)
+        //Metodo que usaremos para asignar el nombre del curso y matricula al alumno.
+        public void AsignarCurso(string curso, int num)
         {
             CursoAsignado = curso;
+            AsignarMatricula(num);
         }
+
+
+        //La funcion de este metodo es reiniciar la matricula del alumno. Ya que puede darse el caso de que el alumno
+        //Decee seleccionar otro curso, en ese caso se deberia asignar una nueva matricula la cual se modificara
+        //Dependiendo del nuevo curso que seleccione el alumno.
+        public void RemoverCurso(string curso)
+        {
+            CursoAsignado = "";
+            Matricula = "";
+        }
+
+        //Usaremos este metodo para controlar la asignacion de una matricula a un alumno.
+        //El metodo recibe un numero que es el numero del alumno dentro del curso que selecciono.
+
+        //El parametro Inombres toma el nombre del usuario y lo convierte en un array de strings
+        //para asi manejar casos en los que el usuario tenga mas de un nombre o el nombre y apellidos.
+
+        //El parametro InicialesNombre verifica si Nombre tiene mas de 2 palabras, si es asi toma las 2 primeras
+        //y selecciona la primera letra de cada palabra. EJ: Nombre = Jose Alfredo. InicialesNombre = JA.
+        //En caso de tener un solo nombre se seleccionan 3 letras del nombre. EJ: Nombre = Alfredo. InicialesNombre = ALF.
+
+        //El parametro Icurso funciona exactamente igual que el Inombres solo que en vez del nombre
+        //Utiliza el Nombre del curso asignado al alumno para hacer la seleccion de letras.
+
+        //Finalmente la matricula se juntan todas las iniciales separadas por guiones y la edad del alumno.
+        private void AsignarMatricula(int num)
+        {
+            string[] Inombres = Nombre.Split(' ');
+            string InicialesNombre = (Inombres.Length > 1) ? string.Concat(Inombres.Take(2).Select(x => x.Substring(0,1).ToUpper())) : Nombre.Substring(0, Math.Min(3, Nombre.Length)).ToUpper();
+
+            string[] Icursos = CursoAsignado.Split(' ');
+            string Iniciales = (Icursos.Length > 1) ? string.Concat(Icursos.Take(2).Select(x => x.Substring(0, 1).ToUpper())) : CursoAsignado.Substring(0, Math.Min(3, CursoAsignado.Length)).ToUpper();
+
+            Matricula = "M-" + InicialesNombre + Edad + "-" + Iniciales + "-" + num.ToString();
+        }
+
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -22,8 +22,25 @@ namespace GestionCursos
         }
         static void Main(string[] args)
         {
+            //Interfaz 1 (ALUMNOS):
+            // Agregar alumno.
+            // Buscar Alumno.
+            // Eliminar alumno.
+            // Mostrar todos los alumnos.
 
-
+            //Interfaz 2 (CURSOS)
+            // Agregar Curso.
+            // Buscar Curso.
+            // Eliminar curso.
+            // Mostrar todos los cursos.
+            
+            //Interfaz 2: (Dentro de un curso)
+            // Ver alumnos del curso.
+            // Inscribir alumno al curso.
+            // Eliminar alumno del curso.
+            // Obtener promedio de alumnos.
+            // Asignar promedio a alumno.
+            // Obtener promedio de todos los alumnos.
 
 
         }
